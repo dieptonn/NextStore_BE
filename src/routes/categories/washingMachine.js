c2f6e42acf7bb2cfd0b8ae5189e49f0efaf82c0b
dieptonn <diep.bt194505@sis.hust.edu.vn>
2024-05-06 21:23:07 +0700

    update

--- src/routes/categories/washingMachine.js
+++ src/routes/categories/washingMachine.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const washingMachineController = require("../../app/controllers/categories/washingMachineController");
 const verifyToken = require("../../app/middlewares/verifyToken");
 
-router.get("/showProduct", verifyToken.verifyToken, washingMachineController.showProduct);
+router.get("/showProduct", washingMachineController.showProduct);
 router.post("/showProductDetail", washingMachineController.showProductDetail);
 
 module.exports = router;
